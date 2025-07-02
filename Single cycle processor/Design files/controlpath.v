8328be2f7c47d152d2970ff59491b84fbce7bc3e
Arun <96939883+RV-Arun22@users.noreply.github.com>
2025-07-03 00:11:01 +0530

    Update controlpath.v with sel_ld for J-type support

--- Single cycle processor/Design files/controlpath.v
+++ Single cycle processor/Design files/controlpath.v
@@ -6,11 +6,11 @@
 // Create Date: 20.06.2025 15:34:44
 // Design Name: 
 // Module Name: controlpath
-// Project Name: 
+// Project Name: RISC-V RV32I Single cycle processor
 // Target Devices: 
 // Tool Versions: 
-// Description: 
-// 
+// Description: Control unit of the processor. Decodes the instruction word and genreates 
+//              required control signals for the sub-blocks.
 // Dependencies: 
 // 
 // Revision:
@@ -23,10 +23,12 @@
 module controlpath(
     input [31:0] instr,         //full instruction from IM, not needed as only a few bits needed by CU
     input z, c, n,              //ALU status flags
-    output reg RF_WEN, DM_WEN,                       //write enables to RF and DM
-    output reg sel_srcB, sel_ld, br_taken,           //srcB, load & branch target(PC source) select signals
-    output reg [1:0] sel_imm,                         //immediate field selecct signals
-    output reg [1:0] sel_s, sel_l, sel_exec_out,     //Exec_unit operation select signals
+    output reg RF_WEN, DM_WEN,                          //write enables to RF and DM
+    output reg sel_srcB,                                //srcB select signal
+    output reg [1:0] sel_ld,                            //RF write (load) in source select signal
+    output reg br_taken,                                //Branch target(PC source) select signal
+    output reg [1:0] sel_imm,                           //immediate field select signals
+    output reg [1:0] sel_s, sel_l, sel_exec_out,        //Exec_unit operation select signals
     output reg sel_a, sel_comp
     );
     wire [6:0] op;          //Opcode
@@ -43,7 +45,7 @@ module controlpath(
                 7'b0110011, 7'b0010011: begin   //R-type & I-type instructions; only op[5] is different
                                 RF_WEN = 1'b1; DM_WEN = 1'b0;   //Writes to RF and not DM 
                                 sel_srcB = op[5]?1'b0 : 1'b1;   //srcB selection based on I or R-type 
-                                sel_ld = 1'b0;                  //Not loading anything from DM
+                                sel_ld = 2'b00;                  //Not loading anything from DM
                                 br_taken = 1'b0;                //No branching in R or I-type, so 0.
                                 sel_imm = op[5]?2'bxx:2'b00;    //if I-type, set imm select lines to 00 
                                 casex({func3, func7_5})
@@ -107,7 +109,7 @@ module controlpath(
                 7'b0000011: begin   // LOAD (I-type) instruction
                                 RF_WEN = 1'b1; DM_WEN = 1'b0;   //Writes to RF and not DM 
                                 sel_srcB = 1'b1;                //srcB needs to be imm
-                                sel_ld = 1'b1;                  //Loading from DM, so load enable is 1.
+                                sel_ld = 2'b10;                  //Loading from DM, so load enable is 1.
                                 br_taken = 1'b0;                //No branching in I-type, so 0.
                                 sel_imm = 2'b00;                //select imm fields for I-type 
                                 case(func3)
@@ -125,7 +127,7 @@ module controlpath(
                                 //case(func3)                   Not needed as I am implementing only store word.
                                 RF_WEN = 1'b0; DM_WEN = 1'b1;   //Writes to DM and not RF
                                 sel_srcB = 1'b1;                //only imm used here
-                                sel_ld = 1'b0;                  //Not loading from DM, so load enable is 0.
+                                sel_ld = 2'b00;                  //Not loading from DM, so load enable is 0.
                                 br_taken = 1'b0;                //No branching in S-type, so 0.
                                 sel_imm = 2'b01;                //select imm fields for S-type
                                 //need to add offset, so alu needs to do add    
@@ -135,7 +137,7 @@ module controlpath(
                 7'b1100011: begin   //B-type instruction
                                 RF_WEN = 1'b0; DM_WEN = 1'b0;   //Writes to neither DM nor RF
                                 sel_srcB = 1'b0;                //only imm used here
-                                sel_ld = 1'b0;                  //Not loading from DM, so load enable is 0.
+                                sel_ld = 2'b00;                  //Not loading from DM, so load enable is 0.
                                 sel_imm = 2'b10;                //select imm fields for B-type
                                 case(func3)
                                     3'b000: begin //beq consition
@@ -151,7 +153,18 @@ module controlpath(
                                                 br_taken = z;                //depends on zero flag     
                                              end 
                                 endcase
-                            end                                               
+                            end
+                7'b1101111: begin   //J-type instruction
+                                RF_WEN = 1'b1; DM_WEN = 1'b0;   //Writes to RF and not DM 
+                                sel_srcB = 1'b1;                //srcB needs to be imm
+                                sel_ld = 2'b01;                  //Not loading anything from DM
+                                br_taken = 1'b1;                //Unconditional branch, branch is always taken.
+                                sel_imm = 2'b11;                //if J-type, set imm select lines to 11 
+                                //ALU doesn't need to do anything, so lets put add controls
+                                sel_a = 'b0; sel_comp = 'bx;sel_s = 'bxx; sel_l = 'bxx;
+                                sel_exec_out = 'b00;                                
+                            end
+                                                                                           
             endcase
         end
 endmodule
